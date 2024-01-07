7cac445810125a600c60c0416ee5da33333cb72d
abdulr2004 <114131077+abdulr2004@users.noreply.github.com>
2024-01-07 10:56:51 +0400

    Delete backend directory

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const app =express()
-
-app.get("/api",(req,res) => {
-  res.json({"users": ["userone","usertwo","userthree"]})
-})
-
-app.listen(5000, ()=>{console.log("Server started on port 5000")})
